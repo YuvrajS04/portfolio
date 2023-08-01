3f7ed33847f36dde1446547696c552c08b33878c
Yuvraj <yuvrajsirohi22@gmail.com>
2023-07-31 22:38:36 -0700

    add partial about section and github logo

--- src/components/Sections/Sections.jsx
+++ src/components/Sections/Sections.jsx
@@ -3,17 +3,24 @@ import "./Sections.scss";
 
 function Sections() {
     return (
-        <section className="hero">
-            <div className="hero__container">
-                <h3 className="hero__subheading">Hi there, I'm</h3>
-                <h1 className="hero__heading">Yuvraj Sirohi.</h1>
-                <h2 className="hero__statement">A Web Developer on a mission.</h2>
-                {/* <h2 className="hero__statement">Crafting Digital Solutions with Precision.</h2>
+        <>
+            <section className="hero">
+                <div className="hero__container">
+                    <h3 className="hero__subheading">Hi there, I'm</h3>
+                    <h1 className="hero__heading">Yuvraj Sirohi.</h1>
+                    <h2 className="hero__statement">A Web Developer on a mission.</h2>
+                    {/* <h2 className="hero__statement">Crafting Digital Solutions with Precision.</h2>
                 <h2 className="hero__statement">Transforming Ideas into Seamless Reality.</h2> */}
-                <p className="hero__paragraph">In the ever-evolving world of web development, I find my passion in turning complex challenges into elegant solutions. With expertise in HTML, CSS, JavaScript, Node, React, and more, I embrace every opportunity to push the boundaries of what can be achieved. Together, let's bring your ideas to life and make a meaningful impact in the digital realm.</p>
-                <a className="hero__button">Discover my work</a>
-            </div>
-        </section>
+                    <p className="hero__paragraph">In the ever-evolving world of web development, I find my passion in turning complex challenges into elegant solutions. With expertise in HTML, CSS, JavaScript, Node, React, and more, I embrace every opportunity to push the boundaries of what can be achieved. Together, let's bring your ideas to life and make a meaningful impact in the digital realm.</p>
+                    <a className="hero__button">Discover my work</a>
+                </div>
+            </section>
+            <section id="about" className="about">
+                <div className="heading__container">
+                    <h2 className="heading"><span className="heading__number" >01.</span>about me</h2>
+                </div>
+            </section>
+        </>
     );
 }
 
