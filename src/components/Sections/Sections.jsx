b0aa1afd9867dbfb3eb07aeb9f302aff31f070dd
Yuvraj <yuvrajsirohi22@gmail.com>
2023-08-01 18:27:50 -0700

    add about me section styling for mobile and tablet

--- src/components/Sections/Sections.jsx
+++ src/components/Sections/Sections.jsx
@@ -1,5 +1,5 @@
 import "./Sections.scss";
-import marker from "../../assets/icons/list-marker.svg";
+import Photo from "../../assets/images/avatar.jpg";
 
 
 function Sections() {
@@ -21,21 +21,27 @@ function Sections() {
                     <h2 className="heading"><span className="heading__number" >01.</span>about me</h2>
                 </div>
                 <div className="about__content-container">
-                <p className="about__content"> Hello! I'm Yuvraj Sirohi, and I have a deep passion for web development and pushing the boundaries of what's possible. My journey in this exciting field began in 2022 when I needed to create a form, which led me to explore the basics of HTML and CSS. I fell in love with the process and haven't looked back since.</p>
-                <p className="about__content">Today, I hold a Web Development Diploma from BrainStation Bootcamp, where I honed my skills and knowledge to become a dedicated developer. My focus is on creating user-centric digital experiences that leave a meaningful impact. I believe in constantly learning and staying up-to-date with the latest technologies to deliver exceptional results.</p>
-                <p className="about__content"> I'm enthusiastic about collaborating on innovative projects and continuing my journey in the dynamic and ever-evolving field of web development. Some of the technologies I've been working with recently include:</p>
-                <ul className="about__skill-list">
-                    <div className="about__list-left">
-                        <li className="about__list-item">JavaScript (ES6+)</li>
-                        <li className="about__list-item">React</li>
-                        <li className="about__list-item">Node.js</li>
+                    <div className="div about__content-wrapper">
+                        <p className="about__content"> Hello! I'm Yuvraj Sirohi, and I have a deep passion for web development and pushing the boundaries of what's possible. My journey in this exciting field began in 2022 when I needed to create a form, which led me to explore the basics of HTML and CSS. I fell in love with the process and haven't looked back since.</p>
+                        <p className="about__content">Today, I hold a Web Development Diploma from BrainStation Bootcamp, where I honed my skills and knowledge to become a dedicated developer. My focus is on creating user-centric digital experiences that leave a meaningful impact. I believe in constantly learning and staying up-to-date with the latest technologies to deliver exceptional results.</p>
+                        <p className="about__content"> I'm enthusiastic about collaborating on innovative projects and continuing my journey in the dynamic and ever-evolving field of web development. Some of the technologies I've been working with recently include:</p>
+                        <ul className="about__skill-list">
+                            <div className="about__list-left">
+                                <li className="about__list-item">JavaScript (ES6+)</li>
+                                <li className="about__list-item">React</li>
+                                <li className="about__list-item">Node.js</li>
+                            </div>
+                            <div className="about__list-right">
+                                <li className="about__list-item">Express</li>
+                                <li className="about__list-item">Web APIs</li>
+                                <li className="about__list-item">OAuth</li>
+                            </div>
+                        </ul>
                     </div>
-                    <div className="about__list-right">
-                        <li className="about__list-item">Express</li>
-                        <li className="about__list-item">Web APIs</li>
-                        <li className="about__list-item">OAuth</li>
+                    <div className="about__image-container">
+                        <img className="about__image about__image-filter" src={Photo} alt="avatar" />
                     </div>
-                </ul>
+
                 </div>
 
             </section>
