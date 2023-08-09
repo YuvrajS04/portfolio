5dc6e35a6009c91d26fc8c4dabb965996cbd7109
Yuvraj Sirohi <yuvrajsirohi22@gmail.com>
2023-08-09 14:06:37 -0700

    create project component and add basic structure and classes

---
+++ src/components/Project/Project.jsx
@@ -0,0 +1,21 @@
+import "./project.scss";
+
+function project() {
+  return ( 
+  <div className="project">
+    <h4 className="project__subheading">Featured Project</h4>
+    <h3 className="project__heading"></h3>
+    <p className="project__description"></p>
+    <ul className="project__tech-stack-list">
+        <li className="tech-stack__list-item"></li>
+    </ul>
+    <a href="" className="project__link">
+        <img src="" alt="Github Logo" className="link__logo" />
+    </a>
+    
+
+  </div>
+  );
+}
+
+export default project;
